794b5175c85b0de5ecf884ce49adef931e490cc3
jordanbeliart <j.beliart.jb@gmail.com>
2019-12-02 15:45:18 +0100

    exo4

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+$name = 'Gérard';
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>PHP Partie1 exo4</title>
+  </head>
+  <body>
+    <p>Bonjour <?php echo $name; ?>, comment vas-tu ?</p>
+  </body>
+</html>
