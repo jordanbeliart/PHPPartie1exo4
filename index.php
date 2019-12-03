69af0eba7dfa950faffa2f62a2b924f3767e28ec
jordanbeliart <j.beliart.jb@gmail.com>
2019-12-03 13:32:56 +0100

    correction

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+//déclaration d'une variable 'name' initialisée 'Gérard'
 $name = 'Gérard';
 ?>
 <!DOCTYPE html>
@@ -8,6 +9,8 @@ $name = 'Gérard';
     <title>PHP Partie1 exo4</title>
   </head>
   <body>
+    <!-- version sans concaténation puis avec concaténation (version à ne pas faire car inutile d'analyser le 'bonjour..., comment vas-tu ?')-->
     <p>Bonjour <?php echo $name; ?>, comment vas-tu ?</p>
+    <p><?php echo 'Bonjour ' . $name . ', comment vas-tu ?'; ?></p>
   </body>
 </html>
